8349ddae48e4fe56a82ce29dbdbbc3874b83b20b
Arushi2002 <79096912+Arushi2002@users.noreply.github.com>
2022-11-28 18:23:37 +0530

    Added more components, images and styling to events page and form page

--- src/form.js
+++ src/form.js
@@ -27,28 +27,40 @@ class Event extends React.Component
     }
     render()
     {
-        
+        const fstyle = {
+            fontSize:"20px",
+            position:"fixed",
+            textAlign:"center",
+            top:"10%",
+            left:"35%",
+            padding:"75px",
+            paddingTop:"50px",
+            background:"#abdbe3",
+            borderStyle:"solid",
+            borderWidth:"2px ",
+            borderColor:"#27a98e"}
 
         
         return(
-            <div>
-                <h2></h2>
-            <form>
-            <p style = {{fontSize:"50px",color:"#1e81b0"}}><b>Register for event</b></p><br/><br/>   
-            Enter Date:<br/><input name = "date" onChange = {this.handleChange} type = "date" size = "40" style = {{height:"50px",fontSize:"30px"}}/>
-            <br/><br/>
-            Phone number:<br/><input type = "tel" name = "phone_num" onChange = {this.handleChange}  size = "40" style = {{height:"50px",fontSize:"30px"}}/>
-            <br/><br/>
-            Email-id<br/><input type="email"  name="mail" size = "40" style = {{height:"50px",fontSize:"30px"}}></input>
-            <br/><br/>
-            Full Name<br/><input type="text"  name= "name" size = "40" style = {{height:"50px",fontSize:"30px"}}></input>
-            <br/><br/>
-
-
             
+            <div style = {fstyle}>
+                <h2></h2>
+            
+            <form>
+            <p style = {{fontSize:"50px",color:"#1e81b0"}}><b>   Register for event</b></p><br/><br/>   
+               Enter Date:<br/><input name = "date" onChange = {this.handleChange} type = "date" size = "30" style = {{height:"40px",fontSize:"30px"}}/>
+            <br/><br/>
+               Phone number:<br/><input type = "tel" name = "phone_num" onChange = {this.handleChange}  size = "30" style = {{height:"40px",fontSize:"30px"}}/>
+            <br/><br/>
+               Email-id<br/><input type="email"  name="mail" size = "30" style = {{height:"40px",fontSize:"30px"}} ></input>
+            <br/><br/>
+               Full Name<br/><input type="text"  name= "name" size = "30" style = {{height:"40px",fontSize:"30px"}}></input>
+            <br/><br/>
+
             
             <button style = {{height:'50px',width:"300px",fontSize:"25px"}} onClick = {this.handleClick}>Buy tickets</button>
             </form>
+            
             </div>)
     }
     
